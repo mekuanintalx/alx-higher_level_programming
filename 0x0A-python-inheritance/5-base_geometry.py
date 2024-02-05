8e3c440fac192a06d57f7d5f3ab22dea9beef1dd
MekuAWS <mekuanint1212@gmail.com>
2024-02-05 09:57:47 +0300

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""basegeometryclass"""
+
+class BaseGeometry:
+    """instance of geometry class"""
+    def __init__(self):
+        """initialize class"""
+        pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""basegeometryclass"""
+
+class BaseGeometry:
+    """instance of geometry class"""
+
+    def __init__(self):
+        """initialize class"""
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
